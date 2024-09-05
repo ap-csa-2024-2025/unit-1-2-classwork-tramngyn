adb65319dba118781c5af4d3de4a679ba7a5bea4
tramngyn <bvnguyen115@student.hbuhsd.edu>
2024-09-05 10:38:16 -0700

    finished homework

--- problem2.java
+++ problem2.java
@@ -1,1 +1,12 @@
-// Create your class here; don't forget the main method
+public class problem2 
+{
+    public static void main(String [] args)
+    {
+        String name1 = "Jamie";
+        String name2 = "Tram";
+        String name3 = "Laura";
+        String name4 = "Zidane";
+        System.out.print(name4 + " " + name2 + " " + name3 + " " + name1);
+    }
+}
+
--- problem3.java
+++ problem3.java
@@ -1,1 +1,10 @@
-// Create your class here; don't forget the main method
+public class problem3 
+{
+    public static void main(String [] args)
+    {
+        String myName = "Tram Nguyen";
+        String myState = "California";
+        System.out.print("My name is " + myName + ". I live in " + myState + ".");
+    }
+}
+
